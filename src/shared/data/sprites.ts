9f5aae4cd8a85fe5fa2910ec60d260150095d8c3
Tony Nie <tonywhitewhite@hotmail.com>
2025-12-04 01:21:06 +0800

    chore: add car and fireball character sprite data
    - Add car character sprite data
    - Add fireball character sprite data
    - Update sprites.ts to include new character sprites

--- src/shared/data/sprites.ts
+++ src/shared/data/sprites.ts
@@ -1,5 +1,7 @@
 import { ACHILLES_SPRITES } from "./characters/achilles"
+import { CAR_SPRITES } from "./characters/car"
 import { FIGHTERFLY_SPRITES } from "./characters/fighter-fly"
+import { FIREBALL_SPRITES } from "./characters/fireball"
 import { LADY_SPRITES } from "./characters/lady"
 import { LUIGI_SPRITES, MARIO_SPRITES } from "./characters/mario"
 import { PENGUIN_SPRITES } from "./characters/penguin"
@@ -14,4 +16,6 @@ export const CHARACTER_SPRITES = [
   ...ACHILLES_SPRITES,
   ...SMILEY_SPRITES,
   ...PENGUIN_SPRITES,
+  ...FIREBALL_SPRITES,
+  ...CAR_SPRITES,
 ]
