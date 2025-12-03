6fb6641c328d862ad5c115c6dfb1f78c9c29ae0a
Tony Nie <tonywhitewhite@hotmail.com>
2025-12-04 00:53:18 +0800

    chore: add new character sprite data files
    - Add penguin character sprite data
    - Add smiley character sprite data
    - Add fighter-fly character sprite data
    - Update sprites.ts to import and export all character sprites

--- src/shared/data/sprites.ts
+++ src/shared/data/sprites.ts
@@ -1,112 +1,17 @@
 import { ACHILLES_SPRITES } from "./characters/achilles"
+import { FIGHTERFLY_SPRITES } from "./characters/fighter-fly"
 import { LADY_SPRITES } from "./characters/lady"
 import { LUIGI_SPRITES, MARIO_SPRITES } from "./characters/mario"
+import { PENGUIN_SPRITES } from "./characters/penguin"
+import { SMILEY_SPRITES } from "./characters/smiley"
 
-const SPRITE_FIGHTERFLY_1_56 = [
-  [0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0],
-  [0,0,1,2,2,0,0,0],
-  [0,1,1,1,0,2,0,2],
-  [0,1,1,1,1,0,3,3],
-  [0,0,1,1,1,3,1,3],
-  [0,0,1,1,1,3,3,3],
-  [0,0,0,0,1,3,3,2],
-]
-
-const SPRITE_FIGHTERFLY_1_57 = [
-  [0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0],
-  [0,0,0,0,2,2,1,0],
-  [2,2,0,2,0,1,1,1],
-  [2,3,3,0,1,1,1,1],
-  [2,3,1,3,1,1,1,1],
-  [2,3,3,3,1,1,1,0],
-  [2,2,3,3,1,0,0,0],
-]
-
-const SPRITE_FIGHTERFLY_1_58 = [
-  [0,0,0,0,0,2,2,1],
-  [0,0,0,0,2,1,3,1],
-  [0,0,0,3,2,1,3,1],
-  [0,0,3,0,0,2,2,2],
-  [0,0,0,0,1,1,3,3],
-  [0,0,2,2,1,0,3,3],
-  [0,2,2,2,2,0,0,3],
-  [0,0,0,0,0,0,0,0],
-]
-
-const SPRITE_FIGHTERFLY_1_59 = [
-  [3,1,2,2,0,0,0,0],
-  [3,1,3,1,2,0,0,0],
-  [3,1,3,1,2,3,0,0],
-  [2,2,2,2,0,0,3,0],
-  [3,3,3,1,0,0,0,0],
-  [3,3,3,0,1,0,0,0],
-  [3,3,0,1,2,2,0,0],
-  [0,0,0,2,2,2,2,0],
-]
-
-const SPRITE_FIGHTERFLY_2_60 = [
-  [0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0],
-  [0,0,0,2,2,0,0,0],
-  [0,0,0,0,0,2,0,2],
-  [0,0,0,0,0,0,3,3],
-  [0,0,0,0,0,3,1,3],
-  [0,0,0,1,1,3,3,3],
-  [0,0,1,1,1,3,3,2],
-]
-
-const SPRITE_FIGHTERFLY_2_61 = [
-  [0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0],
-  [0,0,0,0,2,2,0,0],
-  [2,2,0,2,0,0,0,0],
-  [2,3,3,0,0,0,0,0],
-  [2,3,1,3,0,0,0,0],
-  [2,3,3,3,1,1,0,0],
-  [2,2,3,3,1,1,1,0],
-]
-
-const SPRITE_FIGHTERFLY_2_62 = [
-  [0,1,1,1,1,2,2,1],
-  [0,1,1,1,2,1,3,1],
-  [0,0,1,3,2,1,3,1],
-  [0,0,3,0,0,2,2,2],
-  [0,0,0,0,0,1,3,3],
-  [0,0,0,0,1,0,3,3],
-  [0,0,0,2,2,1,0,3],
-  [0,0,2,2,2,2,0,0],
-]
-
-const SPRITE_FIGHTERFLY_2_63 = [
-  [3,1,2,2,1,1,1,1],
-  [3,1,3,1,2,1,1,1],
-  [3,1,3,1,2,3,1,0],
-  [2,2,2,2,0,0,3,0],
-  [3,3,3,1,1,0,0,0],
-  [3,3,3,0,1,2,2,0],
-  [3,3,0,0,2,2,2,2],
-  [0,0,0,0,0,0,0,0],
-]
 
 export const CHARACTER_SPRITES = [
   ...MARIO_SPRITES,
   ...LUIGI_SPRITES,
   ...LADY_SPRITES,
-  {
-    name: "Fighter Fly (1)",
-    defaultPaletteCode: 2,
-    defaultColorCombination: 1,
-    charCodes: [56, 57, 58, 59],
-    tiles: [SPRITE_FIGHTERFLY_1_56, SPRITE_FIGHTERFLY_1_57, SPRITE_FIGHTERFLY_1_58, SPRITE_FIGHTERFLY_1_59],
-  },
-  {
-    name: "Fighter Fly (2)",
-    defaultPaletteCode: 2,
-    defaultColorCombination: 1,
-    charCodes: [60, 61, 62, 63],
-    tiles: [SPRITE_FIGHTERFLY_2_60, SPRITE_FIGHTERFLY_2_61, SPRITE_FIGHTERFLY_2_62, SPRITE_FIGHTERFLY_2_63],
-  },
+  ...FIGHTERFLY_SPRITES,
   ...ACHILLES_SPRITES,
+  ...SMILEY_SPRITES,
+  ...PENGUIN_SPRITES,
 ]
